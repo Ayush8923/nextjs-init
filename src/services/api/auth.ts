f2610921db5284a65e64eb218bee68acfa3fed51
Ayush <80516839+Ayush8923@users.noreply.github.com>
2025-03-18 12:20:27 +0530

    Initialize feat/verify-email-otp

---
+++ src/services/api/auth.ts
@@ -0,0 +1,32 @@
+import api from "./index";
+
+// Sign In API
+export const signIn = async (email: string, password: string) => {
+  const response = await api.post("/login", {
+    email,
+    password,
+  });
+  return response.data;
+};
+
+// Sign Up API
+export const signUp = async (
+  name: string,
+  email: string,
+  password: string,
+  confirmPassword: string,
+) => {
+  const response = await api.post("/register", {
+    name,
+    email,
+    password,
+    password_confirmation: confirmPassword,
+  });
+  return response.data;
+};
+
+// Verify Otp API
+export const verifyOtp = async (email: string, otp: string) => {
+  const response = await api.post("/verify-otp", { email, otp });
+  return response.data;
+};
