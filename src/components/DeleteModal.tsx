dfcbfd4aa091000d974c0cb40f1cc134e368c201
Ayush8923 <80516839+Ayush8923@users.noreply.github.com>
2025-04-29 23:55:04 +0530

    release(*): 29 Apr

--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -27,7 +27,7 @@ const DeleteModal = ({
       <div className="fixed inset-0 bg-black bg-opacity-50 z-50"></div>
 
       <div className="fixed bottom-0 left-0 right-0 flex justify-center z-50">
-        <div className="bg-white rounded-t-3xl w-full max-w-lg mx-4">
+        <div className="bg-white rounded-t-3xl w-full max-w-lg">
           <div className="flex flex-col px-6 py-9">
             <div className="text-center text-2xl font-medium">{title}</div>
             <div className="mt-6 mb-3 text-base font-normal">{content}</div>
